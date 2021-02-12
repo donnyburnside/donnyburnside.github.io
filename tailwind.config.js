6e73003b29a9b06ec2e5b52bb25bf370c7ac6548
Donny Burnside <donny.burnside@meta.co.uk>
2021-02-12 19:23:07 +0000

    Installed TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './src/content/**/*',
+    './src/includes/**/*',
+    './src/layouts/**/*',
+    './src/scripts/**/*',
+],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
